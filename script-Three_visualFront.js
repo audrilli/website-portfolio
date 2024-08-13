1e6e3723d4f76a3ae175031a94fd6e3685af6c85
audrilli <audreymeret.lohmann@zhdk.ch>
2024-08-14 01:05:32 +0200

    Stacking divs
    not Working (issue with the background link picture)

---
+++ script-Three_visualFront.js
@@ -0,0 +1,12 @@
+console.log("file connecetd");
+
+const container = document.getElementById('front');
+const ContainerColor = document.querySelectorAll('.front');
+
+function mouse (){
+container.addEventListener('mouseover', () =>{
+    container.style('background-color','#000000')
+});
+}
+console.log(container);
+console.log(background-color)
