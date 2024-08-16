7365799909e3adb15e83f591bb4924da8bd0cf2a
audrilli <audreymeret.lohmann@zhdk.ch>
2024-08-16 17:49:53 +0200

    Correct lightening

--- script-Three_visualFront.js
+++ script-Three_visualFront.js
@@ -45,10 +45,10 @@ containerfront.appendChild(rendererfront.domElement);
 // console.log(document.getElementById('landing'))
 
 // Lighting
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 100);
 scene.add(ambientLight);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.set(5, 10, 7.5).normalize();
+const directionalLight = new THREE.DirectionalLight(0xffffff, 100);
+directionalLight.position.set(-10, 0, 7.5).normalize();
 scene.add(directionalLight);
 
 // Physics world
