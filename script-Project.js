d1acb74ec92d80ef4456b7af427756ec78395fa4
audrilli <audreymeret.lohmann@zhdk.ch>
2024-10-27 21:18:46 +0100

    Add interactivity to progress Bar

--- script-Project.js
+++ script-Project.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // const progressBar = document.getElementById("progress");
-    const circles = document.querySelectorAll(".circle");
+  
     const sections = document.querySelectorAll(".trigger-section");
     
     const observerOptions = {
@@ -28,13 +28,74 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }, observerOptions);
 
-    // function updateProgressBar() {
-    //     // Calculate the width of the progress bar based on the active circles
-    //     const activeCircles = document.querySelectorAll(".circle.active").length;
-    //     const progressWidth = ((activeCircles - 1) / (circles.length - 1)) * 100;
-    //     progressBar.style.width = `${progressWidth}%`;
-    // }
-
     // Start observing each section
     sections.forEach(section => observer.observe(section));
 });
+
+//Scrollto sections
+
+const offset = 200;
+
+const briefingButton = document.getElementById("briefing")
+const briefingSection = document.getElementById("briefing-section")
+const briefingSectionTarget = briefingSection.getBoundingClientRect().top + window.scrollY - offset
+
+const ideeButton = document.getElementById("idee")
+const ideeSection = document.getElementById("idee-section")
+const ideeSectionTarget = ideeSection.getBoundingClientRect().top + window.scrollY - offset
+
+const prototypeButton = document.getElementById("prototype")
+const prototypeSection = document.getElementById("prototype-section")
+const prototypeSectionTarget = prototypeSection.getBoundingClientRect().top + window.scrollY - offset
+
+const EntwicklungButton = document.getElementById("development")
+const EntwicklungSection = document.getElementById("development-section")
+const EntwicklungSectionTarget = EntwicklungSection.getBoundingClientRect().top + window.scrollY - offset
+
+const NextStepsButton = document.getElementById("nextsteps")
+const NextStepsSection = document.getElementById("nextsteps-section")
+const NextStepsSectionTarget = NextStepsSection.getBoundingClientRect().top + window.scrollY - offset
+
+
+// Smooth scroll to top on button click
+briefingButton.addEventListener("click", () => {
+    
+    window.scrollTo({
+        top: briefingSectionTarget,
+        behavior: "smooth"
+    })
+  });
+
+  ideeButton.addEventListener("click", () => {
+    
+    window.scrollTo({
+        top: ideeSectionTarget,
+        behavior: "smooth"
+    })
+  });
+
+  prototypeButton.addEventListener("click", () => {
+    
+    window.scrollTo({
+        top: prototypeSectionTarget,
+        behavior: "smooth"
+    })
+  });
+
+
+ EntwicklungButton.addEventListener("click", () => {
+    
+    window.scrollTo({
+        top: EntwicklungSectionTarget,
+        behavior: "smooth"
+    })
+  });
+
+
+  NextStepsButton.addEventListener("click", () => {
+    
+    window.scrollTo({
+        top: NextStepsSectionTarget,
+        behavior: "smooth"
+    })
+  });
