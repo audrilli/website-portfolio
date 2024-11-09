8b70804359853bb93a7d84f27fa9fc12b32720a2
audrilli <audreymeret.lohmann@zhdk.ch>
2024-11-10 00:49:45 +0100

    Add gifs

--- Scripts/script-CardAnimation.js
+++ Scripts/script-CardAnimation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log("connescted")
+
 
 var tinderContainer = document.querySelector('.tinder');
 var allCards = document.querySelectorAll('.tinder--card');
@@ -12,7 +12,7 @@ function initCards(card, index) {
 
   newCards.forEach(function (card, index) {
     card.style.zIndex = allCards.length - index;
-    card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+    card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 20 * index + 'px)';
     card.style.opacity = (10 - index) / 10;
   });
   
