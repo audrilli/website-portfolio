bd7db8baec4a2c1a224e67dc80811ebd79274afa
audrilli <audreymeret.lohmann@zhdk.ch>
2024-11-13 18:16:05 +0100

    Vision Pro Interaction

--- Scripts/script-Three.js
+++ Scripts/script-Three.js
@@ -7,7 +7,7 @@ const height = window.innerHeight;
 const container = document.getElementById("landing");
 
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.5, 1000);
-camera.position.set(0, 0, 1);
+camera.position.set(0, 0, 1.5);
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
@@ -16,16 +16,18 @@ scene.background = null;
 const ambientLight = new THREE.AmbientLight(0xffffff, 2);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(10, 5, 5);
-scene.add(directionalLight);
+// Lights
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 5, 5);
+scene.add(light);
 
 const loaderGLTF = new GLTFLoader();
-const modelScale = 10;
+const modelScale = 1.5;
 
-loaderGLTF.load('media/Flower.gltf', function (gltf) {
+loaderGLTF.load('media/VisionPro1.gltf', function (gltf) {
     model = gltf.scene;
     model.scale.set(modelScale, modelScale, modelScale);
+    model.position.set(0,0.2,0);
 
     model.traverse((child) => {
         if (child.isMesh) {
@@ -41,9 +43,34 @@ loaderGLTF.load('media/Flower.gltf', function (gltf) {
     renderer.render(scene, camera);
 });
 
+// Update light position based on mouse movement
+function onMouseMove(event) {
+    // Convert mouse position to normalized device coordinates (-1 to 1)
+    const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+    const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    // Update light position based on mouse coordinates
+    light.position.set(mouseX * 5, mouseY * 5, 5);
+
+    // Re-render the scene
+    renderer.render(scene, camera);
+}
+
+window.addEventListener('mousemove', onMouseMove);
+
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(width, height);
 container.appendChild(renderer.domElement);
+
+function animate(time) {
+    if (model) {
+        model.rotation.y = time / 2000;
+       
+    }
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
 
 window.addEventListener('resize', () => {
     const newWidth = window.innerWidth;
